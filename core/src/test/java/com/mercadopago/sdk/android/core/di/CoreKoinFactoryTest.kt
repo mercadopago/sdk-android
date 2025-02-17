7c8988e49e54ccd6b0f791bb20d1825a63a0f35a
rodrigmatrix <rodrigo.resende@mercadolivre.com>
2025-02-17 17:55:35 -0300

    fix: fixing unit tests

--- core/src/test/java/com/mercadopago/sdk/android/core/di/CoreKoinFactoryTest.kt
+++ core/src/test/java/com/mercadopago/sdk/android/core/di/CoreKoinFactoryTest.kt
@@ -1,5 +1,10 @@
 package com.mercadopago.sdk.android.core.di
 
+import android.content.Context
+import android.util.Log
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
 import junit.framework.TestCase.assertNotNull
 import org.junit.Test
 import org.koin.core.Koin
@@ -8,7 +13,12 @@ import org.koin.dsl.module
 
 class CoreKoinFactoryTest {
 
-    private class TestModuleProvider(override val koinApp: Koin) : CoreKoinModuleProvider {
+    private class TestModuleProvider : CoreKoinModuleProvider {
+        override val koinApp: Koin = CoreKoinFactory.createKoinApp(
+            provider = this,
+            context = mockk<Context>(relaxed = true),
+        )
+
         override fun provideModules(): List<Module> {
             return listOf(module { /* Definitions for testing */ })
         }
@@ -16,8 +26,13 @@ class CoreKoinFactoryTest {
 
     @Test
     fun `test CoreKoinFactory createKoinApp`() {
+        mockkStatic(Log::class)
+        every { Log.v(any(), any()) } returns 0
+        every { Log.d(any(), any()) } returns 0
+        every { Log.i(any(), any()) } returns 0
+        every { Log.e(any(), any()) } returns 0
         val provider = TestModuleProvider()
-        val koin: Koin = CoreKoinFactory.createKoinApp(provider)
-        assertNotNull(koin)
+
+        assertNotNull(provider.koinApp)
     }
 }
