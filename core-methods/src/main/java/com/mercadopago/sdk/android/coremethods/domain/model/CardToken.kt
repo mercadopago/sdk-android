db9d82e6525b4e79fe0ce5ca1e655b91217f699d
Paixão <carlos.paixao@mercadolivre.com>
2025-05-07 10:43:43 -0300

    fix: changed optional parameters in buyer identification

--- core-methods/src/main/java/com/mercadopago/sdk/android/coremethods/domain/model/CardToken.kt
+++ core-methods/src/main/java/com/mercadopago/sdk/android/coremethods/domain/model/CardToken.kt
@@ -12,14 +12,14 @@ data class CardToken(
  * Buyer Identification
  * @param name is the buyer identification name (To test different payment results, fill
  * in the desired status in the cardholder's name, example: APRO, OTHE, CONT)
- * @param number is the buyer cardholder identification number
- * @param type is the buyer cardholder identification type
+ * @param number (optional) is the buyer cardholder identification number
+ * @param type (optional) is the buyer cardholder identification type
  * @see
  * <a href="https://www.mercadopago.com.br/developers/en/docs/checkout-api/
  * additional-content/your-integrations/test/cards">Test cards</a>
  */
 data class BuyerIdentification(
-    val name: String?,
-    val number: String?,
-    val type: String?
+    val name: String,
+    val number: String? = null,
+    val type: String? = null
 )
