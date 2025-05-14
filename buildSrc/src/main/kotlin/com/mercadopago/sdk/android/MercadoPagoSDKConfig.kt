8748214924f2958938931828d939fc9f7c54f2c7
rodrigmatrix <rodrigo.resende@mercadolivre.com>
2025-05-14 17:33:21 -0300

    feature: added maven publication config

--- buildSrc/src/main/kotlin/com/mercadopago/sdk/android/MercadoPagoSDKConfig.kt
+++ buildSrc/src/main/kotlin/com/mercadopago/sdk/android/MercadoPagoSDKConfig.kt
@@ -9,6 +9,8 @@ import org.gradle.api.JavaVersion
  * @property sourceCompatibility source compatibility
  * @property targetCompatibility target compatibility
  * @property versionName sdk version name
+ * @property NEXUS_NAME nexus repository reference name
+ * @property NEXUS_URL nexus repository url
  * @property RELEASE release
  * @property MAVEN_PUBLISH maven publish
  * @property GROUP_ID group id
@@ -24,6 +26,8 @@ object MercadoPagoSDKConfig {
 
     // SDK Android
     const val versionName = "0.0.1"
+    const val NEXUS_NAME = "sdkAndroid"
+    const val NEXUS_URL = "https://artifacts.mercadolibre.com/repository/android-releases/"
     const val RELEASE = "release"
     const val MAVEN_PUBLISH = "maven-publish"
     const val GROUP_ID = "com.mercadopago.android.sdk"
