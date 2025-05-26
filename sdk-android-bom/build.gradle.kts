3fb21204dccc2dfd7c8c7da40a96fccaee3edef2
rodrigmatrix <rodrigmatrix@gmail.com>
2025-05-26 18:20:17 -0300

    feature: adding bom module

---
+++ sdk-android-bom/build.gradle.kts
@@ -0,0 +1,41 @@
+import com.mercadopago.sdk.android.BomConfig
+
+plugins {
+    id("java-platform")
+    id(MavenConfig.MAVEN_PUBLISH)
+}
+
+dependencies {
+    constraints {
+        api(projects.core)
+        api(projects.analytics)
+        api(projects.sdkAndroid)
+        api(projects.coreMethods)
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>(MavenConfig.RELEASE) {
+            groupId = MavenConfig.GROUP_ID
+            artifactId = BomConfig.ARTIFACT_ID
+            version = BomConfig.VERSION_NAME
+            afterEvaluate {
+                from(components["javaPlatform"])
+                dependencies {
+                    api(projects.sdkAndroid)
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = MavenConfig.NEXUS_NAME
+            credentials {
+                username = System.getenv(MavenConfig.USERNAME)
+                password = System.getenv(MavenConfig.PASSWORD)
+            }
+            url = uri(MavenConfig.NEXUS_URL)
+        }
+    }
+}
