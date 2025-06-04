303aa2e99e60fae5e493c118c1b3f1c465fd2089
capaixao <carlos.paixao@mercadolivre.com>
2025-06-04 16:15:14 -0300

    feature: created foundation module

---
+++ buildSrc/src/main/kotlin/com/mercadopago/sdk/android/FoundationSDKConfig.kt
@@ -0,0 +1,11 @@
+package com.mercadopago.sdk.android
+
+/**
+ * FoundationSDK COnfig
+ * @property ARTIFACT_ID artifact id
+ * @property VERSION_NAME version name
+ */
+object FoundationSDKConfig {
+    const val ARTIFACT_ID = "foundation"
+    const val VERSION_NAME = "0.0.1"
+}
