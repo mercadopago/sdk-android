63b82fb2a6f7f18a4281cf0b65bbdefd003e4ff5
capaixao <carlos.paixao@mercadolivre.com>
2025-06-04 09:11:03 -0300

    feature: created checkout module

---
+++ buildSrc/src/main/kotlin/com/mercadopago/sdk/android/CheckoutSDKConfig.kt
@@ -0,0 +1,11 @@
+package com.mercadopago.sdk.android
+
+/**
+ * CheckoutSDKConfig
+ * @property ARTIFACT_ID artifact id
+ * @property VERSION_NAME version name
+ */
+object CheckoutSDKConfig {
+    const val ARTIFACT_ID = "core-methods"
+    const val VERSION_NAME = "0.0.1"
+}
