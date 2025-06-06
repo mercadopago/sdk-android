eb3cd291d6a6055da7e34b7ffc3e1c0b89b17e41
Paixão <carlos.paixao@mercadolivre.com>
2025-06-06 16:24:41 -0300

    [2777](Feature): Created foundation module (#50)
    * feature: created foundation module
    
    * Update buildSrc/src/main/kotlin/com/mercadopago/sdk/android/FoundationSDKConfig.kt
    
    Co-authored-by: Ismael Caldana <ismael.caldana@mercadolivre.com>
    
    ---------
    
    Co-authored-by: Ismael Caldana <ismael.caldana@mercadolivre.com>

---
+++ buildSrc/src/main/kotlin/com/mercadopago/sdk/android/FoundationSDKConfig.kt
@@ -0,0 +1,11 @@
+package com.mercadopago.sdk.android
+
+/**
+ * FoundationSDK Config
+ * @property ARTIFACT_ID artifact id
+ * @property VERSION_NAME version name
+ */
+object FoundationSDKConfig {
+    const val ARTIFACT_ID = "foundation"
+    const val VERSION_NAME = "0.0.1"
+}
