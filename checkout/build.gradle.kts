abe2712d860146cc85fb59f5eeb1f959b2766519
Paixão <carlos.paixao@mercadolivre.com>
2025-06-09 14:45:12 -0300

    [2773](Feature): Created checkout module (#49)
    * feature: created checkout module
    
    * feature: changed artifact id to checkout
    
    * feature: changed minifyEnabled to true

---
+++ checkout/build.gradle.kts
@@ -0,0 +1,96 @@
+import com.mercadopago.sdk.android.CheckoutSDKConfig
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.klint)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    id(MavenConfig.MAVEN_PUBLISH)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>(MavenConfig.RELEASE) {
+            groupId = MavenConfig.GROUP_ID
+            artifactId = CheckoutSDKConfig.ARTIFACT_ID
+            version = CheckoutSDKConfig.VERSION_NAME
+            afterEvaluate {
+                from(components[MavenConfig.RELEASE])
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = MavenConfig.NEXUS_NAME
+            credentials {
+                username = System.getenv(MavenConfig.USERNAME)
+                password = System.getenv(MavenConfig.PASSWORD)
+            }
+            url = uri(MavenConfig.NEXUS_URL)
+        }
+    }
+}
+
+android {
+    namespace = "com.mercadopago.android.sdk.checkout"
+    compileSdk = MercadoPagoSDKConfig.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = MercadoPagoSDKConfig.MIN_SDK
+        version = CoreMethodsSDKConfig.VERSION_NAME
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    compileOptions {
+        sourceCompatibility = MercadoPagoSDKConfig.sourceCompatibility
+        targetCompatibility = MercadoPagoSDKConfig.targetCompatibility
+    }
+    kotlinOptions {
+        jvmTarget = MercadoPagoSDKConfig.JVM_TARGET
+    }
+}
+
+ksp {
+    arg("skipPrivatePreviews", "true")
+}
+
+dependencies {
+
+    implementation(projects.core)
+    implementation(projects.sdkAndroid)
+    implementation(projects.analytics)
+    implementation(projects.coreMethods)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.showkase)
+    implementation(libs.showkase.annotation)
+    kspDebug(libs.showkase.processor)
+    implementation(libs.androidx.annotation)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
