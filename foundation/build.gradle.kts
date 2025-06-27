890570f01b96e96343f8b5b9b07503a69a4f8094
capaixao <carlos.paixao@mercadolivre.com>
2025-06-27 09:28:36 -0300

    feature: minify false

--- foundation/build.gradle.kts
+++ foundation/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
