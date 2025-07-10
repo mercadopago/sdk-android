55ff723696106e256af7f7e6e97e739ddf089d4f
capaixao <carlos.paixao@mercadolivre.com>
2025-07-10 10:34:36 -0300

    feature: created threeDS SDK module

---
+++ threeds/build.gradle.kts
@@ -0,0 +1,82 @@
+import com.mercadopago.sdk.android.ThreeDSSDKConfig
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.klint)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    id(MavenConfig.MAVEN_PUBLISH)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>(MavenConfig.RELEASE) {
+            groupId = MavenConfig.GROUP_ID
+            artifactId = ThreeDSSDKConfig.ARTIFACT_ID
+            version = ThreeDSSDKConfig.VERSION_NAME
+            afterEvaluate {
+                from(components[MavenConfig.RELEASE])
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = MavenConfig.NEXUS_NAME
+            credentials {
+                username = System.getenv(MavenConfig.USERNAME)
+                password = System.getenv(MavenConfig.PASSWORD)
+            }
+            url = uri(MavenConfig.NEXUS_URL)
+        }
+    }
+}
+
+android {
+    namespace = "com.mercadopago.sdk.android.threeds"
+    compileSdk = MercadoPagoSDKConfig.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = MercadoPagoSDKConfig.MIN_SDK
+        version = CoreMethodsSDKConfig.VERSION_NAME
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = MercadoPagoSDKConfig.sourceCompatibility
+        targetCompatibility = MercadoPagoSDKConfig.targetCompatibility
+    }
+    kotlinOptions {
+        jvmTarget = MercadoPagoSDKConfig.JVM_TARGET
+        allWarningsAsErrors = false
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+        )
+    }
+}
+
+
+ksp {
+    arg("skipPrivatePreviews", "true")
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
