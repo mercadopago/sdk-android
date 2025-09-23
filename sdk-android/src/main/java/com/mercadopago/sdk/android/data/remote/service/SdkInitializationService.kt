6fffb65321fe2533965a155c75e1afa0aab5feab
capaixao <capaixao@users.noreply.github.com>
2025-09-23 17:31:20 -0300

    fix: fixed site id route

--- sdk-android/src/main/java/com/mercadopago/sdk/android/data/remote/service/SdkInitializationService.kt
+++ sdk-android/src/main/java/com/mercadopago/sdk/android/data/remote/service/SdkInitializationService.kt
@@ -3,10 +3,11 @@ package com.mercadopago.sdk.android.data.remote.service
 import com.mercadopago.sdk.android.data.remote.response.SiteIdResponse
 import retrofit2.http.GET
 
-private const val BRICKS_API = "bricks_api"
+private const val BRICKS_API = "cho-off"
+private const val VERSION = "v1"
 
 internal interface SdkInitializationService {
 
-    @GET("$BRICKS_API/site_id")
+    @GET("$BRICKS_API/$VERSION/site_id")
     suspend fun fetchSiteId(): SiteIdResponse
 }
