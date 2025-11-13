b2c9c02c04045e160c8b9fc8ab3b9cd36a03f7a5
capaixao <capaixao@users.noreply.github.com>
2025-11-13 09:13:14 -0300

    feature: created input component

---
+++ components/src/main/java/com/mercadopago/sdk/android/components/MPInput.kt
@@ -0,0 +1,410 @@
+package com.mercadopago.sdk.android.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.cardnumber.CardNumberTextField
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.cardnumber.CardNumberTextFieldEvent
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.expirationdate.ExpirationDateFormat
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.expirationdate.ExpirationDateTextField
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.expirationdate.ExpirationDateTextFieldEvent
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.pcitextfield.PCIFieldState
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.pcitextfield.rememberPCIFieldState
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.securitycode.SecurityCodeTextField
+import com.mercadopago.sdk.android.coremethods.ui.components.textfield.securitycode.SecurityCodeTextFieldEvent
+import com.mercadopago.sdk.android.coremethods.ui.utils.MaskVisualTransformationDefaults
+import com.mercadopago.sdk.android.foundation.theme.MercadoPagoTheme
+
+enum class LabelState {
+    Idle,
+    Disabled,
+    Error
+}
+
+@Composable
+fun MPCardNumberTextField(
+    modifier: Modifier = Modifier,
+    state: PCIFieldState,
+    onEvent: (CardNumberTextFieldEvent) -> Unit,
+    isFocused: Boolean = false,
+    showPlaceHolder: Boolean = false,
+    error: Boolean = false,
+    enabled: Boolean = true,
+    label: String? = null,
+    helper: String? = null,
+    visualTransformation: VisualTransformation = MaskVisualTransformationDefaults.CardNumber,
+) {
+    MPInputBody(
+        error = error,
+        enabled = enabled,
+        label = label,
+        helper = helper,
+    ) {
+        CardNumberTextField(
+            state = state,
+            modifier = modifier.fillMaxWidth(),
+            onEvent = onEvent,
+            textStyle = MercadoPagoTheme.typography.body.mediumRegular,
+            enabled = enabled,
+            visualTransformation = visualTransformation,
+            decorationBox = { innerTextField ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        // Adding a border by field state created for this example
+                        .addBorder(
+                            isFocused = isFocused,
+                            error = error
+                        )
+                        .height(OutlinedTextFieldDefaults.MinHeight)
+                        .padding(horizontal = 16.dp),
+                ) {
+                    Box {
+                        if (showPlaceHolder) {
+                            MPText(
+                                text = "123",
+                                textStyle = MPTextStyle.BodyMediumRegular,
+                                modifier = Modifier.align(Alignment.CenterStart),
+                            )
+                        }
+                        innerTextField()
+                    }
+                }
+            },
+        )
+    }
+}
+
+@Composable
+fun MPExpirationDateTextField(
+    modifier: Modifier = Modifier,
+    state: PCIFieldState,
+    onEvent: (ExpirationDateTextFieldEvent) -> Unit,
+    dateFormat: ExpirationDateFormat = ExpirationDateFormat.ShortFormat,
+    isFocused: Boolean = false,
+    showPlaceHolder: Boolean = false,
+    error: Boolean = false,
+    enabled: Boolean = true,
+    label: String? = null,
+    helper: String? = null,
+) {
+    MPInputBody(
+        error = error,
+        enabled = enabled,
+        label = label,
+        helper = helper,
+    ) {
+        ExpirationDateTextField(
+            state = state,
+            modifier = modifier.fillMaxWidth(),
+            enabled = enabled,
+            dateFormat = dateFormat,
+            onEvent = onEvent,
+            textStyle = MercadoPagoTheme.typography.body.mediumRegular,
+            decorationBox = { innerTextField ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .addBorder(
+                            isFocused = isFocused,
+                            error = error
+                        )
+                        .height(OutlinedTextFieldDefaults.MinHeight)
+                        .padding(horizontal = 16.dp),
+                ) {
+                    Box {
+                        if (showPlaceHolder) {
+                            MPText(
+                                text = "123",
+                                textStyle = MPTextStyle.BodyMediumRegular,
+                                modifier = Modifier.align(Alignment.CenterStart),
+                            )
+                        }
+                        innerTextField()
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun MPSecurityCodeTextField(
+    modifier: Modifier = Modifier,
+    state: PCIFieldState,
+    onEvent: (SecurityCodeTextFieldEvent) -> Unit,
+    securityCodeSize: Int = 3,
+    isFocused: Boolean = false,
+    showPlaceHolder: Boolean = false,
+    error: Boolean = false,
+    enabled: Boolean = true,
+    label: String? = null,
+    helper: String? = null,
+) {
+    MPInputBody(
+        error = error,
+        enabled = enabled,
+        label = label,
+        helper = helper,
+    ) {
+        SecurityCodeTextField(
+            state = state,
+            modifier = modifier.fillMaxWidth(),
+            onEvent = onEvent,
+            enabled = enabled,
+            textStyle = MercadoPagoTheme.typography.body.mediumRegular,
+            securityCodeSize = securityCodeSize,
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+            decorationBox = { innerTextField ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .addBorder(
+                            isFocused = isFocused,
+                            error = error
+                        )
+                        .height(OutlinedTextFieldDefaults.MinHeight)
+                        .padding(horizontal = 16.dp),
+                ) {
+                    Box(modifier = Modifier.weight(1f)) {
+                        if (showPlaceHolder) {
+                            MPText(
+                                text = "123",
+                                textStyle = MPTextStyle.BodyMediumRegular,
+                                modifier = Modifier.align(Alignment.CenterStart),
+                            )
+                        }
+                        innerTextField()
+                    }
+                    Spacer(Modifier.width(4.dp))
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = null,
+                        modifier = Modifier.size(34.dp),
+                    )
+                }
+            },
+        )
+    }
+}
+
+@Composable
+internal fun MPInputBody(
+    error: Boolean = false,
+    enabled: Boolean = true,
+    label: String? = null,
+    helper: String? = null,
+    showHelperIcon: Boolean = false,
+    icon: ImageVector? = null,
+    content: @Composable () -> Unit,
+) {
+    val state =
+        if (error) LabelState.Error else if (!enabled) LabelState.Disabled else LabelState.Idle
+    Column {
+
+        label?.let {
+            MPLabel(
+                it,
+                modifier = Modifier.padding(start = MercadoPagoTheme.spacing.xxs),
+                textStyle = MPTextStyle.BodySmallRegular,
+                labelState = state
+            )
+        }
+        content()
+        helper?.let {
+            MPHelper(
+                text = it,
+                modifier = Modifier.padding(start = MercadoPagoTheme.spacing.xxs),
+                labelState = state,
+                showIcon = showHelperIcon,
+                icon = icon
+            )
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun MPDropList(
+    text: String,
+    modifier: Modifier = Modifier,
+    dropList: List<String>
+) {
+    var expanded by remember { mutableStateOf(false) }
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryEditable),
+        ) {
+            MPText(
+                text = text,
+                modifier = modifier.widthIn(min = 32.dp)
+            )
+            ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+        }
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            dropList.forEach { option ->
+                DropdownMenuItem(
+                    text = {
+                        MPText(text = option)
+                    },
+                    onClick = {
+                        expanded = false
+                        //onSelectIdentification(option)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+internal fun MPHelper(
+    text: String,
+    modifier: Modifier = Modifier,
+    showIcon: Boolean = false,
+    labelState: LabelState = LabelState.Idle,
+    icon: ImageVector? = null
+) {
+    Row {
+        if (showIcon) icon?.let {
+            Icon(it, "")
+            Spacer(modifier = Modifier.padding(start = MercadoPagoTheme.spacing.xxs))
+        }
+        MPLabel(
+            text,
+            modifier = modifier,
+            labelState = labelState,
+            textStyle = MPTextStyle.BodyExtraSmallSemiBold
+        )
+    }
+}
+
+@Composable
+internal fun MPLabel(
+    text: String,
+    modifier: Modifier = Modifier,
+    textStyle: MPTextStyle = MPTextStyle.Title,
+    labelState: LabelState = LabelState.Idle
+) {
+    val colorType = when (labelState) {
+        LabelState.Idle -> MPTextColorType.Primary
+        LabelState.Disabled -> MPTextColorType.Inverted
+        LabelState.Error -> MPTextColorType.Negative
+    }
+
+    MPText(
+        text,
+        modifier = modifier,
+        textStyle = textStyle,
+        colorType = colorType
+    )
+}
+
+@Composable
+internal fun Modifier.addBorder(
+    isFocused: Boolean,
+    error: Boolean = false,
+): Modifier {
+    return border(
+        width = if (isFocused) 2.dp else 1.dp,
+        color = if (error) {
+            MaterialTheme.colorScheme.error
+        } else if (isFocused) {
+            MaterialTheme.colorScheme.primary
+        } else {
+            MaterialTheme.colorScheme.outline
+        },
+        shape = MaterialTheme.shapes.small,
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MPSecurityCodeTextFieldPreview() {
+    MercadoPagoTheme {
+        val securityCodeState = rememberPCIFieldState()
+        Column(
+            modifier = Modifier.padding(10.dp)
+        ) {
+            MPSecurityCodeTextField(
+                state = securityCodeState,
+                onEvent = {
+
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MPCardNumberTextFieldPreview() {
+    MercadoPagoTheme {
+        val cardNumberState = rememberPCIFieldState()
+        Column(
+            modifier = Modifier.padding(10.dp)
+        ) {
+            MPCardNumberTextField(
+                state = cardNumberState,
+                onEvent = {
+
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MPExpirationDateTextFieldPreview() {
+    MercadoPagoTheme {
+        val expirationDateState = rememberPCIFieldState()
+        Column(
+            modifier = Modifier.padding(10.dp)
+        ) {
+            MPExpirationDateTextField(
+                state = expirationDateState,
+                onEvent = {
+
+                }
+            )
+        }
+    }
+}
