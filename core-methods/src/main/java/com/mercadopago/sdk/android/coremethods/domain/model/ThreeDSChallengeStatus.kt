fa10929c5e673262779e51229a340a303be6106e
capaixao <capaixao@users.noreply.github.com>
2025-12-30 15:36:04 -0300

    feature: detekt changes

--- core-methods/src/main/java/com/mercadopago/sdk/android/coremethods/domain/model/ThreeDSChallengeStatus.kt
+++ core-methods/src/main/java/com/mercadopago/sdk/android/coremethods/domain/model/ThreeDSChallengeStatus.kt
@@ -1,6 +1,5 @@
 package com.mercadopago.sdk.android.coremethods.domain.model
 
-
 internal enum class ThreeDSChallengeStatus(val value: String) {
     COMPLETED("COMPLETED"),
     CANCELLED("CANCELLED"),
--- core-methods/src/main/java/com/mercadopago/sdk/android/coremethods/domain/provider/ThreeDSProviderManager.kt
+++ core-methods/src/main/java/com/mercadopago/sdk/android/coremethods/domain/provider/ThreeDSProviderManager.kt
@@ -1,6 +1,5 @@
 package com.mercadopago.sdk.android.coremethods.domain.provider
 
-
 internal class ThreeDSProviderManager {
     @Volatile
     private var provider: ThreeDSProvider? = null
