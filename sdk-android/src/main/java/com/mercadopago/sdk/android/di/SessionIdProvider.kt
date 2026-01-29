4deb5eb77096611d4e1f556e1bfff754e1c69bc7
capaixao <capaixao@users.noreply.github.com>
2026-01-29 09:47:22 -0300

    feature: ktlint

--- sdk-android/src/main/java/com/mercadopago/sdk/android/di/SessionIdProvider.kt
+++ sdk-android/src/main/java/com/mercadopago/sdk/android/di/SessionIdProvider.kt
@@ -4,9 +4,16 @@ import androidx.annotation.RestrictTo
 
 /**
  * Provides the session identifier for the current SDK instance.
+ *
+ * Used internally to correlate requests and track session lifecycle within the SDK.
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 interface SessionIdProvider {
 
+    /**
+     * Returns the unique session identifier for the current SDK instance.
+     *
+     * @return The session ID string.
+     */
     fun getSessionId(): String
 }
