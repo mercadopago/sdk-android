8ba2a9b7abd64cd95a1d5d196b14d177fa10c972
samantaalbanez <saah_albanez@hotmail.com>
2026-02-11 10:08:56 -0300

    align with iOS

---
+++ components/src/main/java/com/mercadopago/sdk/android/components/extensions/AnyExtensions.kt
@@ -0,0 +1,13 @@
+package com.mercadopago.sdk.android.components.extensions
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+@OptIn(ExperimentalContracts::class)
+fun Any?.isNotNull(): Boolean {
+    contract {
+        returns(true) implies (this@isNotNull != null)
+    }
+
+    return this != null
+}
