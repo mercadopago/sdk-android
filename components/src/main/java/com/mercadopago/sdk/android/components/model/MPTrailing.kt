361c8bd1e6d6daaa38bb5c49cceb062919823a63
samantaalbanez <saah_albanez@hotmail.com>
2026-02-11 10:05:15 -0300

    Revert "align with iOS"
    This reverts commit ece76e0ce8dae2166d9a22b3608168899144c8fa.

--- components/src/main/java/com/mercadopago/sdk/android/components/model/MPTrailing.kt
+++ components/src/main/java/com/mercadopago/sdk/android/components/model/MPTrailing.kt
@@ -7,12 +7,14 @@ import androidx.compose.ui.graphics.vector.ImageVector
  * Trailing model class, used to determine the trailing showing type
  * This its used to change the component showed in trailing
  *
+ * @param type: Trailing type [MPTrailingType]
  * @param text: Trailing text
  * @param icon: Trailing icon
  * @param textColor: Trailing text color
  */
 data class MPTrailing(
-    val text: String? = null,
+    val type: MPTrailingType,
+    val text: String,
     val icon: ImageVector? = null,
     val textColor: Color? = null,
 )
---
+++ components/src/main/java/com/mercadopago/sdk/android/components/model/MPTrailingType.kt
@@ -0,0 +1,17 @@
+package com.mercadopago.sdk.android.components.model
+
+/**
+ * Trailing Type enum class, used to determine the trailing showing type
+ * This its used to change the component showed in trailing
+ */
+enum class MPTrailingType {
+    /**
+     *  Text: Trailing type that`s shows a text
+     */
+    Text,
+
+    /**
+     *  Pill: Trailing type that`s shows a pill
+     */
+    Pill,
+}
