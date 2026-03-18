92f32b60efe6caf20275e760ec1018670bc4b23d
samantaalbanez <saah_albanez@hotmail.com>
2026-03-18 19:49:51 -0300

    translations

---
+++ checkout/src/main/java/com/mercadopago/sdk/android/checkout/domain/mapper/CountryCodeToLocaleMapper.kt
@@ -0,0 +1,20 @@
+package com.mercadopago.sdk.android.checkout.domain.mapper
+
+import com.mercadopago.sdk.android.domain.model.CountryCode
+import java.util.Locale
+
+internal object CountryCodeToLocaleMapper {
+    fun map(
+        countryCode: CountryCode?,
+    ): Locale =
+        when (countryCode) {
+            CountryCode.BRA -> Locale("pt", "BR")
+            CountryCode.ARG -> Locale("es", "AR")
+            CountryCode.MEX -> Locale("es", "MX")
+            CountryCode.URY -> Locale("es", "UY")
+            CountryCode.COL -> Locale("es", "CO")
+            CountryCode.PER -> Locale("es", "PE")
+            CountryCode.CHL -> Locale("es", "CL")
+            else -> Locale.getDefault()
+        }
+}
