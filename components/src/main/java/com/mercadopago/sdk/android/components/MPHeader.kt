e5aa382764b192f7fc0c6381c3cf64a596f0640f
samantaalbanez <saah_albanez@hotmail.com>
2026-03-26 14:42:24 -0300

    adjust title header

--- components/src/main/java/com/mercadopago/sdk/android/components/MPHeader.kt
+++ components/src/main/java/com/mercadopago/sdk/android/components/MPHeader.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mercadopago.sdk.android.components.extensions.scrollProgressRatio
@@ -107,6 +108,7 @@ private fun MPHeaderExpandedTitle(
         MPText(
             text = title,
             style = MercadoPagoAndesTheme.typography.heading.default.huge,
+            fontWeight = FontWeight.Bold,
         )
         if (subtitle.isNotBlank()) {
             Spacer(modifier = Modifier.size(MercadoPagoAndesTheme.spacing.paddings.xmicro))
