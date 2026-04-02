5790d96a5d79255cdaa73ba6f8f70a107f7588ab
samantaalbanez <saah_albanez@hotmail.com>
2026-04-02 16:11:13 -0300

    add service

---
+++ checkout/src/main/java/com/mercadopago/sdk/android/checkout/data/remote/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.mercadopago.sdk.android.checkout.data.remote.utils
+
+import com.mercadopago.android.sdk.checkout.BuildConfig
+
+internal const val PRODUCT_ID: String = BuildConfig.CHECKOUT_PRODUCT_ID
