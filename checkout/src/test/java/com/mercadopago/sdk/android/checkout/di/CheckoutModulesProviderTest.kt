0bb0950338fb6b90cbe2c0bd32ade674f778e8cd
capaixao <capaixao@users.noreply.github.com>
2026-04-08 15:29:13 -0300

    fix: changed cardform to not nulable

--- checkout/src/test/java/com/mercadopago/sdk/android/checkout/di/CheckoutModulesProviderTest.kt
+++ checkout/src/test/java/com/mercadopago/sdk/android/checkout/di/CheckoutModulesProviderTest.kt
@@ -3,6 +3,7 @@ package com.mercadopago.sdk.android.checkout.di
 import android.app.Application
 import android.content.pm.ApplicationInfo
 import android.content.res.Configuration
+import com.mercadopago.sdk.android.checkout.core.model.CardFormConfiguration
 import com.mercadopago.sdk.android.checkout.core.model.CheckoutType
 import com.mercadopago.sdk.android.checkout.core.model.internal.CheckoutConfiguration
 import com.mercadopago.sdk.android.checkout.data.preferences.CheckoutThemePreferences
@@ -75,7 +76,7 @@ internal class CheckoutModulesProviderTest {
 
         // When
         val checkoutConfiguration = CheckoutConfiguration(
-            checkoutType = CheckoutType.CardForm(),
+            checkoutType = CheckoutType.CardForm(CardFormConfiguration()),
             paymentMethods = emptyList(),
         )
         val module = module {
