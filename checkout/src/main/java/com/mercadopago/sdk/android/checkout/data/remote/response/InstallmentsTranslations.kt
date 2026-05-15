57ce1ce2e12197dd337987c81d29962727a4d0a1
capaixao <capaixao@users.noreply.github.com>
2026-05-15 16:12:12 -0300

    chore(3917): card-bin BFF DTO additive changes (1/4)
    Backward-compat DTOs:
    - Translations: + currencySymbol (default "")
    - InstallmentsTranslations: + payButtonLabel (default ""); keep interestFreeLabel (tech-debt, removed in 4/4)
    - CardBinResponse.QuotaResponse: + primaryLabel/secondaryLabel/tertiaryLabel/state (nullable defaults)
    
    Refs: 3917

--- checkout/src/main/java/com/mercadopago/sdk/android/checkout/data/remote/response/InstallmentsTranslations.kt
+++ checkout/src/main/java/com/mercadopago/sdk/android/checkout/data/remote/response/InstallmentsTranslations.kt
@@ -2,6 +2,8 @@ package com.mercadopago.sdk.android.checkout.data.remote.response
 
 internal data class InstallmentsTranslations(
     val header: InstallmentsHeaderTranslations,
+    // tech-debt: campo será removido na PR 4/4 do split 3917 quando consumidores migrarem para payButtonLabel
     val interestFreeLabel: String,
     val totalLabel: String,
+    val payButtonLabel: String = "",
 )
