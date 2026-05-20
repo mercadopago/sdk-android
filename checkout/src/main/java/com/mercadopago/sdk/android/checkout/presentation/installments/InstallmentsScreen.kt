b0fbc1eab4e5dec0539409d5af4e61a48f638460
capaixao <capaixao@users.noreply.github.com>
2026-05-20 09:24:04 -0300

    fix: changed to clear and add semantic

--- checkout/src/main/java/com/mercadopago/sdk/android/checkout/presentation/installments/InstallmentsScreen.kt
+++ checkout/src/main/java/com/mercadopago/sdk/android/checkout/presentation/installments/InstallmentsScreen.kt
@@ -19,9 +19,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.role
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mercadopago.sdk.android.checkout.presentation.state.FooterState
@@ -129,7 +129,7 @@ private fun ChevronInstallmentItem(
         ),
         modifier = Modifier
             .fillMaxWidth()
-            .semantics(mergeDescendants = true) {
+            .clearAndSetSemantics {
                 contentDescription = item.accessibilityLabel
                 role = Role.Button
             },
@@ -155,7 +155,7 @@ private fun RadioButtonInstallmentItem(
         ),
         modifier = Modifier
             .fillMaxWidth()
-            .semantics(mergeDescendants = true) {
+            .clearAndSetSemantics {
                 contentDescription = item.accessibilityLabel
                 role = Role.Button
             },
