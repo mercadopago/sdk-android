af2166112472fd6d165ed7dd77e6fdc25919722a
samantaalbanez <saah_albanez@hotmail.com>
2026-05-22 10:17:17 -0300

    testapp

--- example/src/main/java/com/mercadopago/sdk/android/example/presentation/checkout/CheckoutExampleScreen.kt
+++ example/src/main/java/com/mercadopago/sdk/android/example/presentation/checkout/CheckoutExampleScreen.kt
@@ -45,11 +45,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mercadopago.sdk.android.checkout.core.MercadoPagoCheckout
-import com.mercadopago.sdk.android.checkout.core.model.Order
 import com.mercadopago.sdk.android.checkout.core.model.CheckoutType
 import com.mercadopago.sdk.android.checkout.core.model.PaymentMethod
 import android.widget.Toast
 import com.mercadopago.sdk.android.checkout.domain.callback.MercadoPagoCheckoutResult
+import com.mercadopago.sdk.android.checkout.domain.model.MPPaymentData
 import com.mercadopago.sdk.android.checkout.domain.model.UserCancelledContext
 import com.mercadopago.sdk.android.example.presentation.theme.MercadoPagoSampleTheme
 import kotlinx.coroutines.launch
@@ -69,7 +69,7 @@ internal fun CheckoutExampleScreen(
     val checkout = remember {
         MercadoPagoCheckout.Builder(
             context = context,
-            checkoutType = CheckoutType.CardTransaction(Order()),
+            checkoutType = CheckoutType.CardSave,
         ).setPaymentMethods(listOf(PaymentMethod.Card()))
             .build()
     }
@@ -106,8 +106,13 @@ internal fun CheckoutExampleScreen(
                     onRegisterCard = {
                         checkout.show { result ->
                             when (result) {
-                                is MercadoPagoCheckoutResult.Success ->
-                                    state = CheckoutState.Success(result.paymentData.token)
+                                is MercadoPagoCheckoutResult.Success -> {
+                                    val token = when (val data = result.paymentData) {
+                                        is MPPaymentData.CardSave -> data.token
+                                        is MPPaymentData.CardTransaction -> ""
+                                    }
+                                    state = CheckoutState.Success(token)
+                                }
 
                                 is MercadoPagoCheckoutResult.Error ->
                                     Toast.makeText(
